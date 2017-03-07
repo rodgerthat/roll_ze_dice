c360534d57d9e6d118bce95cc240bfcfa130c280
joelrnorris <joel.r.norris@gmail.com>
2017-03-07 11:30:10 -0600

    product game finished. ish.

--- roll_ze_dice/roll_ze_dice.cpp
+++ roll_ze_dice/roll_ze_dice.cpp
@@ -23,7 +23,15 @@ int main()
 	srand(time(0));		// seed the random number generator
 
 	int numDice = 0;	// how many dice does the user wanna roll?
-	int theAnswer = 0;	// something to store the answer in
+	int theAnswer = 1;	// something to store the answer in, whatever you do, don't make this a 0 to start...
+	int userScore = 0;	// nowhere to go but up.
+	int userAnswer = 0;	// 
+
+	// output catchy gaem titles
+	cout << "********************************" << endl;
+	cout << "* Roll ze Dice : Product Game! *" << endl;
+	cout << "********************************" << endl;
+	cout << endl;
 
 	do {
 		
@@ -32,6 +40,8 @@ int main()
 
 		cin >> numDice ;	// get user input
 
+		theAnswer = 1;		// reset the answer
+
 		for (int i=0; i < numDice; ++i) {
 			
 			// for the number of selected dice, give 'em a roll.
@@ -39,9 +49,26 @@ int main()
 			diceArray[i].roll();
 			theAnswer *= diceArray[i].getSide();	// calculate ze answer whilst we rollolololol
 
+			// more debuggery
+			//cout << endl <<  diceArray[i].getSide() << endl;
+			//cout << endl << theAnswer << endl;
 		}
 
-		// query the user for ze answer!
+		do {
+			// query the user for ze answer!
+			cout << endl;
+			cout << "What is the product of these dice? ";
+			cin >> userAnswer;
+			
+			// for some old skool debuggery
+			//cout << "theAnswer : " << theAnswer << endl;
+			//cout << "userAnswer : " << userAnswer << endl;
+
+		} while ( theAnswer != userAnswer );
+
+		cout << "YAY! Correct!" << endl;
+		userScore += theAnswer;
+		cout << "You're score is : " << userScore << endl;
 		
 		cout << endl;
 		cout << "Roll ze dice again? y/n : ";
